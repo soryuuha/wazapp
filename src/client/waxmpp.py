d3580775c22a6926765948fe6dd852751998cb09
CODeRUS <coderusinbox@gmail.com>
2012-12-04 02:02:38 +0600

    Handle blocked contacts in group chat

--- src/client/waxmpp.py
+++ src/client/waxmpp.py
@@ -378,16 +378,18 @@ class WAEventHandler(QObject):
 		def wrapped(self, *args):
 			messageId = args[0]
 			jid = args[1]
-			for blockedJid in self.blockedContacts.split(","):
-				if blockedJid == jid:
-					self.interfaceHandler.call("message_ack", (jid, messageId))
+			
+			try:
+				if '-' in jid:
+					self.blockedContacts.index(args[2]);
+					self._d("BLOCKED MESSAGE FROM " + args[2])
+				else:
+					self.blockedContacts.index(jid);
 					self._d("BLOCKED MESSAGE FROM " + jid)
-					return
-			if WAXMPP.message_store.messageExists(jid, messageId):
-				self.interfaceHandler.call("message_ack", (jid, messageId))
+				#self.interfaceHandler.call("message_ack", (jid, messageId))
 				return
-
-
+			except ValueError:
+				pass
 
 			key = Key(jid,False,messageId);
 			msg = WAXMPP.message_store.createMessage(jid)
