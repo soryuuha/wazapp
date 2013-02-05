49006e05cbdec63fff4e2e9a662fa82b5854aa0f
CODeRUS <coderusinbox@gmail.com>
2013-02-05 23:41:50 +0600

    Returning resending unsent messages.

--- src/client/waxmpp.py
+++ src/client/waxmpp.py
@@ -316,15 +316,7 @@ class WAEventHandler(QObject):
 			Resends all unsent messages, should invoke on connect
 		'''
 
-
 		messages = WAXMPP.message_store.getUnsent();
-		#self._d("Resending %i old messages"%(len(messages)))
-		self._d("Skipping %i old messages"%(len(messages)))
-		for m in messages:
-			m.status = 1
-			m.save()
-			
-		'''
 		self._d("Resending %i old messages"%(len(messages)))
 		for m in messages:
 			media = m.getMedia()
@@ -363,7 +355,7 @@ class WAEventHandler(QObject):
 				except:
 					self._d("skipped sending an old message because of i don't know why!")
 
-		self._d("Resending old messages done")'''
+		self._d("Resending old messages done")
 
 	def getDisplayPicture(self, jid = None):
 		picture = "/opt/waxmppplugin/bin/wazapp/UI/common/images/user.png"
